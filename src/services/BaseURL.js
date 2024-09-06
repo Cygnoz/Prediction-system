c50c46ca43997995721ac5ce5a6088272496da4d
AkhilMSajeevan <162311402+AkhilMSajeevan@users.noreply.github.com>
2024-09-06 16:23:04 +0530

    Update BaseURL.js

--- src/services/BaseURL.js
+++ src/services/BaseURL.js
@@ -1,4 +1,4 @@
 //const BASE_URL = 'https://13.232.52.232:5000';
-const BASE_URL = 'https://127.0.0.1:5000';
+const BASE_URL = 'http://192.168.20.7:5000';
 //const BASE_URL = 'https://prediction.aisana.net';
 export default BASE_URL;
