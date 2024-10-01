9659dddbf1cb09069d9dedf4d87c5bad16188e57
A-k-hil <akhilsajeevan@cygnonex.com>
2024-10-01 16:58:45 +0530

    Update start-server.mjs

--- start-server.mjs
+++ start-server.mjs
@@ -1,35 +1,58 @@
+// import { createServer } from 'http';
+
+// import serve from 'serve-handler';
+ 
+// const server = createServer((req, res) => {
+
+//   return serve(req, res, {
+
+//     public: 'build',
+
+//     rewrites: [
+
+//       { source: '**', destination: '/index.html' }
+
+//     ]
+
+//   }).catch(err => {
+
+//     console.error('Error serving files:', err);
+
+//     res.writeHead(500, { 'Content-Type': 'text/plain' });
+
+//     res.end('Internal Server Error');
+
+//   });
+
+// });
+ 
+// const port = process.env.PORT || 3000;
+
+// server.listen(port, () => {
+
+//   console.log(`Server is running on http://localhost:${port}`);
+
+// });
+
 import { createServer } from 'http';
-
 import serve from 'serve-handler';
- 
+
 const server = createServer((req, res) => {
-
   return serve(req, res, {
-
     public: 'build',
-
     rewrites: [
-
       { source: '**', destination: '/index.html' }
-
     ]
-
   }).catch(err => {
-
     console.error('Error serving files:', err);
-
     res.writeHead(500, { 'Content-Type': 'text/plain' });
-
     res.end('Internal Server Error');
-
   });
-
 });
- 
+
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-
   console.log(`Server is running on http://localhost:${port}`);
-
 });
+
