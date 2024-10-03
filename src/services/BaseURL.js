caf02df60b52ec5cc3ab15b917417b0aaae6e97f
Akhil Sajeevan <akhilsajeevan@cygnonex.com>
2024-10-03 09:47:39 +0530

    base url changed

--- src/services/BaseURL.js
+++ src/services/BaseURL.js
@@ -1,4 +1,4 @@
 //const BASE_URL = 'https://13.232.52.232:5000';
-const BASE_URL = 'https://127.0.0.1:5000';
-//const BASE_URL = 'https://prediction.aisana.net';
+//const BASE_URL = 'https://127.0.0.1:5000';
+const BASE_URL = 'https://prediction.aisana.net:5000';
 export default BASE_URL;
